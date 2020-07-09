8060e61b1dc9eaafc460bfbb38466a7396d6d833
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-09 20:17:15 -0300

    Aprendendo como criar templates

--- resources/views/admin/Config.blade.php
+++ resources/views/admin/Config.blade.php
@@ -1,16 +1,23 @@
-<h1>CONFIGURAÇÕES</h1>
 
-Meu nome é {{ $name }} meu email é {{$email}}
+@extends('layouts.admin')
+@section('title', 'Configurações')
 
-Versão:{{$versao}}
+@section('content')
 
-<form method="post">
-    @csrf
-    <label for="name">Nome</label>
-    <input type="text" name="name"/>
-    <label for="email">Email</label>
-    <input type="text" name="email"/>
-    <input type="submit" value="Enviar">
-</form>
+    <h1>CONFIGURAÇÕES</h1>
 
-<a href="config/info">Informações</a>
+    Meu nome é {{ $name }} meu email é {{$email}}
+
+    Versão:{{$versao}}
+
+    <form method="post">
+        @csrf
+        <label for="name">Nome</label>
+        <input type="text" name="name"/>
+        <label for="email">Email</label>
+        <input type="text" name="email"/>
+        <input type="submit" value="Enviar">
+    </form>
+
+    <a href="config/info">Informações</a>
+@endsection
