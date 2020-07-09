de89dce951a4e637dc2534dc2dc10a3939f80183
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-09 19:56:46 -0300

    Dados Globais

--- resources/views/admin/Config.blade.php
+++ resources/views/admin/Config.blade.php
@@ -2,13 +2,14 @@
 
 Meu nome é {{ $name }} meu email é {{$email}}
 
+Versão:{{$versao}}
+
 <form method="post">
     @csrf
     <label for="name">Nome</label>
     <input type="text" name="name"/>
     <label for="email">Email</label>
     <input type="text" name="email"/>
-
     <input type="submit" value="Enviar">
 </form>
 
